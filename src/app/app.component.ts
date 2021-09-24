c9e08131187900021eb1583a68faedbe11ebd5c6
Laramiadell <71432940+Laramiadell@users.noreply.github.com>
2021-09-24 14:54:09 +0800

    first commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularApp';
+  title ='Angular Random Number Calculator App'
+  developer = 'Lara Mia Dell R. Lara'
 }
